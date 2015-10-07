146f4ea81e69de9f38554b6e5cbe1e46c1c83c2e
Geert Huls <geert.huls@macadam.eu>
2015-10-07 22:19:23 +0200

    Initial gulp bundle task setup - using reactify to transpile jsx into plain js - using browserify to generate js code - using vinyl source stream to output js code into app.js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
 var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var reactify = require('reactify');
 
 gulp.task('live-server', function () {
 	var server = new LiveServer('server/main.js');
 	server.start();
+});
+
+gulp.task('bundle', function () {
+	return browserify({
+		entries: 'app/main.jsx',
+		debug: true,
+	})
+	//Chain the browserify to the reactify transform.
+	//The reactify transforms the jsx into plain javascript.
+	.transform(reactify)
+	//Bundle using browserify bundle command.
+	//This wraps up the transformations and tells browserify
+	//we're ready to output the file.
+	.bundle()
+	//Vinyl source stream will spit out the new js file name:
+	//Before transformation filename is called main,
+	//afterward it will be called app.
+	.pipe(source('app.js'))
+	.pipe(gulp.dest('./.tmp'));
+
 });
 
 gulp.task('serve', /*depends on:*/['live-server'], function () {
