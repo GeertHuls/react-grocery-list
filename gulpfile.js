cf02a432560fd9b1fe3a7a22c0f20d12eeb1a640
Geert Huls <geert.huls@macadam.eu>
2015-10-08 22:41:25 +0200

    Created the strikethrough visual

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('live-server', function () {
 	server.start();
 });
 
-gulp.task('bundle', function () {
+gulp.task('bundle', ['copy'], function () {
 	return browserify({
 		entries: 'app/main.jsx',
 		debug: true,
@@ -29,6 +29,12 @@ gulp.task('bundle', function () {
 	.pipe(gulp.dest('./.tmp'));
 
 });
+
+gulp.task('copy', function () {
+	gulp.src(['app/*.css'])
+	.pipe(gulp.dest('./.tmp'));
+})
+
 					//bundling always happens when serve taks is fired
 gulp.task('serve', /*depends on:*/['bundle', 'live-server'], function () {
 		browserSync.init(null, {
