ce48a9cf58119a9f4a3b35fa2bfa1f97b529d5a8
Geert Huls <geert.huls@macadam.eu>
2015-10-13 22:02:39 +0200

    Installed body parser allowing the app to proccess requests other than get

--- server/main.js
+++ server/main.js
@@ -2,11 +2,18 @@ var express = require('express');
 
 var app = new express();
 
+var parser = require('body-parser');
+
 app.get('/',function(req,res){
     res.render('./../app/index.ejs',{});
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
+
+//This allows the express application to process json requests:
+app.use(parser.json());
+//This allows to handle post requests:
+app.use(parser.urlencoded({extended:false}));
 
 //Register the items route:
 require('./routes/items.js')(app);
